d0cf9da4ab0d24cc81601d3043f685b39533f59c
suhaybkaofficial <suhaybmaxamuud55@gmail.com>
2024-07-12 15:42:53 +0300

    first commit

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=200, blank=True)  # New field for image URL
+
+    def __str__(self):
+        return self.name
