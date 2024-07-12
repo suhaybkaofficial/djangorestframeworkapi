9f4a2e7b10fb7ecd02f9b5fc502ba7bc6c0b0ea1
suhaybkaofficial <suhaybmaxamuud55@gmail.com>
2024-07-12 17:17:06 +0300

    updated the static root

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(STATIC_URL, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
